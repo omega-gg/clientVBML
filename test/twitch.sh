2d5036e1b3427597befa7eed0388a674ebb62d00
bunjee <bunjee@omega.gg>
2023-04-15 14:31:04 +0200

    test: Update twitch

--- test/twitch.sh
+++ test/twitch.sh
@@ -30,6 +30,9 @@ test "TRACK" "https://www.twitch.tv/videos/1171797190"
 
 test "TRACK LIVE" "https://www.twitch.tv/esl_sc2"
 
+test "PLAYLIST A" "https://www.twitch.tv/directory/all"
+test "PLAYLIST B" "https://www.twitch.tv/directory/collection/recently-released-games-streams"
+
 test "SEARCH CHANNELS" "vbml://run?backend=twitch&method=search&label=tracks&q=starcraft"
 
 test "SEARCH VIDEOS" "vbml://run?backend=twitch&method=search&label=videos&q=starcraft"
