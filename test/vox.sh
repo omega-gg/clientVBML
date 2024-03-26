92e290020f465f8d83f0b97e62f610d530f84024
bunjee <bunjee@omega.gg>
2024-03-26 16:17:58 +0100

    test/vox: Add playlist

--- test/vox.sh
+++ test/vox.sh
@@ -30,6 +30,8 @@ test "TRACK" "https://vox.omega.gg/w/9d5fYXTcjLHReVUQabKRwB"
 
 test "FEED" "https://vox.omega.gg/c/vox"
 
+test "PLAYLIST" "https://vox.omega.gg/w/p/ehXUsZrq4TJZVBC8J7GWpN"
+
 test "TRENDING" "vbml:run?backend=vox&method=view&q=vox.omega.gg/trending"
 
 test "RELATED" "vbml:run?backend=vox&method=related&label=tracks&q=vox.omega.gg/w/9d5fYXTcjLHReVUQabKRwB"
